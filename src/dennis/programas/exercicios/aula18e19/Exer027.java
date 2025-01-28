f9937a69c9bd5c88a18731462b8556505274f880
Dennis-Viturino <dennis.andrade.viturino@ccc.ufcg.edu.br>
2025-01-28 00:38:48 -0300

    mais uma aula sobre arrays

--- src/dennis/programas/exercicios/aula18e19/Exer027.java
+++ src/dennis/programas/exercicios/aula18e19/Exer027.java
@@ -8,6 +8,39 @@ public class Exer027 {
         Scanner sc = new Scanner(System.in);
 
         
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("Digite o valor do vetor A na posição " + (i+1));
+            vetorA[i] = sc.nextInt();
+
+            if (vetorA[i] < 7) {
+                vetorB[i] = 'a';
+            } else if (vetorA[i] == 7) {
+                vetorB[i] = 'b';
+            } else if (vetorA[i] > 7 && vetorA[i] < 10) {
+                vetorB[i] = 'c';
+            } else if (vetorA[i] == 10) {
+                vetorB[i] = 'd';
+            } else if (vetorA[i] > 10) {
+                vetorB[i] = 'e';
+            }
+
+        }
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println(); 
+
 
     }
 }
