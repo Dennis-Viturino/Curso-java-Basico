3299e9d3a61866a7613f332888a4f82048373c04
Dennis-Viturino <dennis.andrade.viturino@ccc.ufcg.edu.br>
2025-02-24 07:12:28 -0300

    Subindo aulas de herança

---
+++ src/dennis/programas/aula37/Teste.java
@@ -0,0 +1,22 @@
+package src.dennis.programas.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+       
+        /* 
+        Aluno aluno = new Aluno();
+
+        //Professor professor = new Professor();
+
+        //Pessoa pessoa = new Pessoa();
+
+        // como aluno extends pessoa, ele tem acesso aos metodos de pessoa
+        aluno.setNome("Maria");
+        aluno.getNome();
+
+        */
+
+        // como pessoa é uma super classe, é possivel fazer isso mas so te macesso a atributos de pessoa
+        // Pessoa aluno = new Aluno();
+    }
+}
---
+++ src/dennis/programas/aula38/Teste.java
@@ -0,0 +1,9 @@
+package src.dennis.programas.aula38;
+
+public class Teste {
+    public static void main(String[] args) {
+        
+        
+
+    }
+}
